bcee691d0d782d782f318e2c74b61b9a21f7e7d1
byork6 <byork041@outlook.com>
2024-10-05 13:28:02 -0500

    Login page formatting complete. Issue #1

--- src/handicapcalculator/pages/login.py
+++ src/handicapcalculator/pages/login.py
@@ -1,11 +1,10 @@
 # 3rd-party libraries
 import toga
 from toga.style import Pack
-from toga.style.pack import COLUMN, ROW, CENTER, BOLD, RIGHT, BOTTOM, LEFT
+from toga.style.pack import COLUMN, ROW, CENTER, BOLD, BOTTOM, RIGHT
 # Built-in libraries
 from typing import Callable, LiteralString
 import os
-# User-defined libraries
 
 
 class LoginPage:
@@ -28,43 +27,30 @@ class LoginPage:
         self.username_input: toga.TextInput | None = None
         self.content: toga.Box | None = None
         self.logged_in: bool = False
-        self.usga_image_file_path: LiteralString | str | bytes | None = self.get_file_path('WHS.png')
-        self.usga_image: toga.ImageView | None = self.create_login_image(self.usga_image_file_path)
+        self.usga_image_file_path: LiteralString | str | bytes | None = None
+        self.usga_image: toga.ImageView | None = None
+
         return
-
-    @staticmethod
-    def get_file_path(filename: str) -> LiteralString | str | bytes | None:
-        filepath: LiteralString | str | bytes | None = None
-        try:
-            filepath = os.path.join(os.path.dirname(__file__), filename)
-        except OSError:
-            print(f"Error: {filename} not found.")
-        return filepath
-
-    def create_login_image(self, filepath: LiteralString | str | bytes | None) -> toga.ImageView | None:
-        try:
-            self.usga_image = toga.ImageView(id='images', image=self.usga_image_file_path)
-        except FileNotFoundError:
-            print(f"Error: Failed to load image")
-            return None
-        return self.usga_image
 
     def create(self, login_action_callback) -> toga.Box:
         self.login_callback = login_action_callback
-        self.content = toga.Box(style=Pack(direction=COLUMN))
+        # vertical spacing adjusted here (horizontal spacing adjusted in each child box creation method)
+        self.content = toga.Box(style=Pack(direction=COLUMN, alignment=CENTER, flex=2, padding=(10, 0, 10, 0), height=600))
+
+        # create child boxes
+        self.usga_image_file_path = self.get_file_path("WHS.png")
+        self.usga_image = self.create_login_image(self.usga_image_file_path)
         self.username_input = self.create_username_input()
         self.password_input = self.create_password_input()
         self.login_failed_message = self.create_login_failed_message()
+        self.login_button = self.create_login_button()
 
-        # TODO: Fix image sizing and position
+        # add children boxes to parent box
         if self.usga_image is not None:
             self.content.add(self.usga_image)
-
         self.content.add(self.username_input)
         self.content.add(self.password_input)
         self.content.add(self.login_failed_message)
-
-        self.login_button = self.create_login_button()
         self.content.add(self.login_button)
 
         return self.content
@@ -73,10 +59,10 @@ class LoginPage:
     def create_username_input() -> toga.Box:
         username_label = toga.Label(
             "Username: ",
-            style=Pack(padding=(300, 0, 5, 175), flex=1, width=100, font_weight=BOLD, font_size=10),
+            style=Pack(padding=1, flex=1, font_weight=BOLD, font_size=10),
         )
-        username_text_input = toga.TextInput(style=Pack(flex=2, font_size=10))
-        username_input_box = toga.Box(style=Pack(direction=ROW, padding=5, flex=0, width=467, alignment=BOTTOM))
+        username_text_input = toga.TextInput(style=Pack(flex=5, font_size=10, width=200))
+        username_input_box = toga.Box(style=Pack(direction=ROW, padding=(0, 200, 5, 200), flex=5, width=300))
         username_input_box.add(username_label)
         username_input_box.add(username_text_input)
 
@@ -86,10 +72,10 @@ class LoginPage:
     def create_password_input() -> toga.Box:
         password_label = toga.Label(
             "Password: ",
-            style=Pack(padding=(0, 0, 5, 175), flex=1, width=100, font_weight=BOLD, font_size=10),
+            style=Pack(padding=1, flex=1, font_weight=BOLD, font_size=10),
         )
-        password_input = toga.PasswordInput(style=Pack(flex=2, font_size=10))
-        password_input_box = toga.Box(style=Pack(direction=ROW, padding=5, flex=0, width=467, alignment=CENTER))
+        password_input = toga.PasswordInput(style=Pack(flex=5, font_size=10, width=200))
+        password_input_box = toga.Box(style=Pack(direction=ROW, padding=(0, 200, 0, 200), flex=5, width=300))
         password_input_box.add(password_label)
         password_input_box.add(password_input)
 
@@ -99,14 +85,14 @@ class LoginPage:
     def create_login_failed_message() -> toga.Label:
         return toga.Label(
             "Login Failed",
-            style=Pack(padding=(0, 5), color="red", visibility="hidden", flex=1),
+            style=Pack(padding=5, color="red", visibility="hidden", flex=4),
         )
 
     def create_login_button(self) -> toga.Button:
         return toga.Button(
             "Login",
             on_press=self.validate_login,
-            style=Pack(direction=ROW, padding=(5,20,20,175), flex=2, width=300, alignment=CENTER, font_size=10, font_weight=BOLD),
+            style=Pack(direction=ROW, padding=(1, 1, 20, 1), flex=1, font_size=10, font_weight=BOLD, width=300),
         )
 
     def validate_login(self, _widget) -> None:
@@ -123,3 +109,22 @@ class LoginPage:
     @staticmethod
     def check_login_credentials(username, password) -> bool:
         return True if username == "user" and password == "password" else False
+
+    @staticmethod
+    def get_file_path(filename: str) -> LiteralString | str | bytes | None:
+        filepath: LiteralString | str | bytes | None = None
+        try:
+            filepath = os.path.join(os.path.dirname(__file__), filename)
+        except OSError:
+            print(f"Error: {filename} not found.")
+        return filepath
+
+    def create_login_image(self, filepath: LiteralString | str | bytes | None) -> toga.ImageView | None:
+        try:
+            self.usga_image = toga.ImageView(id='images', image=self.usga_image_file_path,
+                                             style=Pack(padding=0, flex=50, width=600)
+                                             )
+        except FileNotFoundError:
+            print(f"Error: Failed to load image")
+            return None
+        return self.usga_image
